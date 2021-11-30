d5ecd89c5d1276550f935aa2de9f80c73e4e6698
Ethan Perkins <ethanperkinspro@gmail.com>
2021-11-30 10:27:20 -0700

    first commit

---
+++ iTunesSearchApp/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  iTunesSearchApp
+//
+//  Created by Ethan Perkins on 11/30/21.
+//
+
+import Foundation
---
+++ iTunesSearchApp/SearchResultsController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultsController.swift
+//  iTunesSearchApp
+//
+//  Created by Ethan Perkins on 11/30/21.
+//
+
+import Foundation
