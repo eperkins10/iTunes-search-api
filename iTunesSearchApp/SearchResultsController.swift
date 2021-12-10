372334b3fd16d5bb40fa40a45e567c00dc8ce0fd
Ethan Perkins <ethanperkinspro@gmail.com>
2021-12-10 11:52:46 -0700

    updated

--- iTunesSearchApp/SearchResultsController.swift
+++ iTunesSearchApp/SearchResultsController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class SearchResultsController {
+    
+    static func searchResultsForSearchTerm(searchTerm: String, completion: @escaping (_ results: [SearchResult]?) -> Void) {
+        
+        let urlString = "https://itunes.apple.com/search?term=\(searchTerm)"
+        guard let url = URL(string: urlString) else { return }
+        
+        fetchJSONAtURL(url: url) { (json) in
+            guard let j = json,
+                  let allResults = j["results"] as? [[String: AnyObject]] else { return }
+            let searchResults = allResults.compactMap({ return SearchResult(json: $0) })
+            completion(searchResults)
+        }
+    }
+    
+    static func fetchJSONAtURL(url: URL, completion: @escaping (_ json: [String : AnyObject]?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let e = error {
+                print("Error downloading data \(e)")
+            } else if let d = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: d, options: []) as? [String: AnyObject] {
+                        completion(json)
+                    }
+                } catch let jsonError {
+                    print("error deconding with JSON \(jsonError)")
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- iTunesSearchApp/ViewController.swift
+++ iTunesSearchApp/ViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+        guard let term = searchTextField.text else { return }
+        
+        SearchResultsController.searchResultsForSearchTerm(searchTerm: term) { [weak self] (results) in
+            if let searchResults = results,
+               let firstResult = searchResults.first {
+                DispatchQueue.main.async {
+                    self?.resultsLabel.text = "\(firstResult.trackName) - \(firstResult.artistName)"
+                }
+            }
+        }
+    }
+    
+    
 
 
 }
